97ddd74c309e9299bb42b59cab46433d74f15406
Iulian-Marian BARBU (78306) <iulian_marian.barbu@stud.acs.upb.ro>
2019-02-24 17:29:43 +0200

    added functions for changing firefox versions (31,41)

--- scripts/begin.sh
+++ scripts/begin.sh
@@ -8,8 +8,12 @@ export PATH=$PATH:$NUTCH_RUNTIME_HOME/bin:$SOLR_INSTALLATION/bin:$HBASE_HOME/bin
 export ORIG_PATH=$PATH
 PATH=/home/iulian/Licenta/firefox-41.0-x86_64:$ORIG_PATH
 
-function switchToOldFirefox() {
+function switchTo41Firefox() {
 	PATH=/home/iulian/Licenta/firefox-41.0-x86_64:$ORIG_PATH
+}
+
+function switchTo31Firefox() {
+	PATH=/home/iulian/Licenta/firefox-31.4.0esr-x86_64:$ORIG_PATH
 }
 
 function switchToNewFirefox() {
