8e519260bb7b92cec732bff3f627e74a73dff568
Iulian-Marian BARBU (78306) <iulian_marian.barbu@stud.acs.upb.ro>
2019-05-06 01:29:36 +0300

    you idiot :(

--- scripts/begin.sh
+++ scripts/begin.sh
@@ -4,19 +4,3 @@ export NUTCH_HOME=/home/iulian/Licenta/nutch
 export NUTCH_RUNTIME_HOME=$NUTCH_HOME/runtime/local
 export SOLR_HOME=/home/iulian/Licenta/solr-7.3.1/server/solr
 export SOLR_INSTALLATION_HOME=$SOLR_HOME/../../
-
-export PATH=$PATH:$HADOOP_HOME:$NUTCH_RUNTIME_HOME/bin:$SOLR_INSTALLATION_HOME/bin
-
-export ORIG_PATH=$PATH
-
-function switchTo41Firefox() {
-	PATH=/home/iulian/Licenta/firefox-41.0-x86_64:$ORIG_PATH
-}
-
-function switchTo31Firefox() {
-	PATH=/home/iulian/Licenta/firefox-31.4.0esr-x86_64:$ORIG_PATH
-}
-
-function switchToNewFirefox() {
-	PATH=$ORIG_PATH
-}
